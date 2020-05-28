cc9b2abf4b8f1af01acaba58c400b145428512cc
[yuruiqi] <[yuruiqi0516@gmail.com]>
2020-05-28 12:42:17 +0800

    update

--- Model.py
+++ Model.py
@@ -6,9 +6,9 @@ import torch.nn as nn
 # ResNet
 class ResBlock(nn.Module):
     """
-    residual block used in the ResNet
+    Construct Residual block used in the ResNet.
     """
-    def __init__(self, in_channels, filters, stride=(1, 1), res_conv=False):
+    def __init__(self, in_channels, filters, stride=1, res_conv=False):
         """
         in_channels: the channel number of input tensor
         filters: [n_filter1, n_filter2, n_filter3], the filter number of the three conv blocks
@@ -18,15 +18,15 @@ class ResBlock(nn.Module):
         super().__init__()
         self.res_conv = res_conv
 
-        self.conv1 = nn.Sequential(nn.Conv2d(in_channels, filters[1], kernel_size=1, stride=stride),
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels, filters[0], kernel_size=1, stride=stride),
+                                   nn.BatchNorm2d(filters[0]),
+                                   nn.ReLU())
+        self.conv2 = nn.Sequential(nn.Conv2d(filters[0], filters[1], kernel_size=3, padding=1),
                                    nn.BatchNorm2d(filters[1]),
                                    nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(filters[1], filters[2], kernel_size=3, padding=1),
-                                   nn.BatchNorm2d(filters[2]),
-                                   nn.ReLU())
-        self.conv3 = nn.Sequential(nn.Conv2d(filters[2], filters[3], kernel_size=1),
-                                   nn.BatchNorm2d(filters[3]))
-        self.conv1x1 = nn.Conv2d(in_channels, filters[3], kernel_size=1, stride=stride)
+        self.conv3 = nn.Sequential(nn.Conv2d(filters[1], filters[2], kernel_size=1),
+                                   nn.BatchNorm2d(filters[2]))
+        self.conv1x1 = nn.Conv2d(in_channels, filters[2], kernel_size=1, stride=stride)
         self.relu = nn.ReLU()
 
     def forward(self, x):
@@ -44,29 +44,29 @@ class ResBlock(nn.Module):
 
 class ResNet(nn.Module):
     """
-     a ResNet structure.
-    Return: [C1, C2, C3, C4, C5]. featuremaps with different depth
+    Construct ResNet.
+    Return: [C1, C2, C3, C4, C5]. feature maps with different depth
     """
     def __init__(self, in_channels, out_channels):
         """
         in_channels: the channel number of the input tensor.
         out_channels: the channel number of the output tensor. Then the filter numbers of the 5 stages will be
-                      out_channels/32, out_channels/8, out_channels/4, out_channels/2, out_channels respectively.
-                      It's 2048 in Mask R-CNN.
+                      out_channels/32, out_channels/8, out_channels/4, out_channels/2, out_channels respectively
+                      ('//' is used to avoid errors raised because of the float type). It's 2048 in Mask R-CNN.
         """
         super().__init__()
         # stage 1
-        self.stage1 = nn.Sequential(nn.Conv2d(in_channels, out_channels/32, kernel_size=3, stride=2, padding=3),
+        self.stage1 = nn.Sequential(nn.Conv2d(in_channels, out_channels//32, kernel_size=7, stride=2, padding=3),
                                     nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
         # stage 2
-        self.stage2 = self.construct_stage(out_channels/32, out_channels/8, conv_stride=(1, 1), n_blocks=2)
+        self.stage2 = self.construct_stage(out_channels//32, out_channels//8, conv_stride=(1, 1), n_blocks=2)
         # stage 3
-        self.stage2 = self.construct_stage(out_channels/8, out_channels/4, conv_stride=(2, 2), n_blocks=3)
+        self.stage3 = self.construct_stage(out_channels//8, out_channels//4, conv_stride=(2, 2), n_blocks=3)
         # stage 4
-        self.stage4 = self.construct_stage(out_channels/4, out_channels/2, conv_stride=(2, 2), n_blocks=5)
+        self.stage4 = self.construct_stage(out_channels//4, out_channels//2, conv_stride=(2, 2), n_blocks=5)
         # stage 5
         # block_num should be 5 for resnet50 and 22 for resnet101.
-        self.stage5 = self.construct_stage(out_channels/2, out_channels, conv_stride=(2, 2), n_blocks=2)
+        self.stage5 = self.construct_stage(out_channels//2, out_channels, conv_stride=(2, 2), n_blocks=2)
 
     def construct_stage(self, in_channels, out_channels, conv_stride, n_blocks):
         """
@@ -79,46 +79,46 @@ class ResNet(nn.Module):
 
         return: a sequence of the blocks
         """
-        layers = [ResBlock(in_channels, [out_channels/4, out_channels/4, out_channels],
+        # add identity block
+        layers = [ResBlock(in_channels, [out_channels//4, out_channels//4, out_channels],
                            stride=conv_stride, res_conv=True)]
 
+        # add conv blocks
         for i in range(n_blocks):
-            layers.append(ResBlock(out_channels, [out_channels/4, out_channels/4, out_channels]))
+            layers.append(ResBlock(out_channels, [out_channels//4, out_channels//4, out_channels]))
 
         return nn.Sequential(*layers)
 
     def forward(self, x):
         c1 = self.stage1(x)
-        c2 = self.stage2(x)
-        c3 = self.stage3(x)
-        c4 = self.stage4(x)
-        c5 = self.stage5(x)
-        return [c1, c2, c3,c4, c5]
+        c2 = self.stage2(c1)
+        c3 = self.stage3(c2)
+        c4 = self.stage4(c3)
+        c5 = self.stage5(c4)
+        return [c1, c2, c3, c4, c5]
 
 
 # FPN
 class FPN(nn.Module):
     """
-    Construnct Feature Pyramid Network
+    Construct Feature Pyramid Network.
     input: [c1, c2, c3, c4, c5]
-    output: [p2, p3, p4, p5, p6]
+    return: [p2, p3, p4, p5, p6]
     """
     def __init__(self, in_channels, out_channels):
         """
-        in_channels: the channel number of the input tensor.
+        in_channels: the channel number of the input tensor. Default to be 2048 for c5. So it'll be 1024, 512, 156 for
+                    c4, c3, c2 respectively.
         out_channels: the channel number of the output tensor. It's 256 in Mask R-CNN.
         """
         super().__init__()
-        self.conv1_1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.conv1_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-
-        self.conv2_1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv2_1 = nn.Conv2d(in_channels//8, out_channels, kernel_size=1)
         self.conv2_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
 
-        self.conv3_1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv3_1 = nn.Conv2d(in_channels//4, out_channels, kernel_size=1)
         self.conv3_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
 
-        self.conv4_1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv4_1 = nn.Conv2d(in_channels//2, out_channels, kernel_size=1)
         self.conv4_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
 
         self.conv5_1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
@@ -128,7 +128,9 @@ class FPN(nn.Module):
     def forward(self, featuremaps):
         _, c2, c3, c4, c5 = featuremaps
         p5 = self.conv5_1(c5)
-        p4 = self.conv4_1(c4) + nn.Upsample(scale_factor=2)(p5)
+        p4 = self.conv4_1(c4)
+        p4up = nn.Upsample(scale_factor=2)(p5)
+        # p4 = self.conv4_1(c4) + nn.Upsample(scale_factor=2)(p5)
         p3 = self.conv3_1(c3) + nn.Upsample(scale_factor=2)(p4)
         p2 = self.conv2_1(c2) + nn.Upsample(scale_factor=2)(p3)
 
@@ -140,4 +142,90 @@ class FPN(nn.Module):
 
         return [p2, p3, p4, p5, p6]
 
+
+# RPN
+class RPN(nn.Module):
+    """
+    Construct Region Proposal Network.
+    input: [batch, channels, height, width], a feature map from P2 to P6 in turn
+    return: [rpn_class_logits, rpn_probs, rpn_bbox], the score and delta of every anchors
+    """
+    def __init__(self, in_channels, anchor_stride=1, anchors_per_location=9):
+        """
+        in_channels: the channel number of the input tensor
+        anchor_stride: the stride of the pixel to get anchors. Default to be 1.
+        anchors_per_location: the number of anchors gotten from one pixel. Default to be 9.
+        """
+        super().__init__()
+        self.conv_share = nn.Sequential(nn.Conv2d(in_channels, 512, kernel_size=3, padding=1, stride=anchor_stride),
+                                        nn.ReLU())
+        self.conv_class = nn.Conv2d(512, 2*anchors_per_location, kernel_size=1)
+        self.conv_bbox = nn.Conv2d(512, 4*anchors_per_location, kernel_size=1)
+
+    def forward(self, x):
+        """
+        Returns:
+            rpn_class_logits: [batch, H * W * anchors_per_location, 2] Anchor classifier logits (before softmax)
+            rpn_probs: [batch, H * W * anchors_per_location, 2] Anchor classifier probabilities.
+            rpn_bbox: [batch, H * W * anchors_per_location, 4] Deltas (dy, dx, log(dh), log(dw))
+                     to be applied to anchors.
+        """
+        shared = self.conv_share(x)
+
+        # Anchor Score. [batch, height, width, anchors per location * 2]
+        rpn_class_logits = self.conv_class(shared)
+        # Reshape to [batch, anchors, 2]
+        rpn_class_logits = rpn_class_logits.reshape([rpn_class_logits.shape[0], -1, 2])
+        # Softmax on last dimension of BG/FG.
+        rpn_probs = nn.functional.softmax(rpn_class_logits)
+
+        # Bounding box refinement delta. [batch, H, W, anchors per location * depth]
+        # the four elements means [x, y, log(w), log(h)]
+        rpn_bbox = self.conv_bbox(shared)
+        # Reshape to [batch, anchors, 4]
+        rpn_bbox = rpn_bbox.reshape([rpn_bbox.shape[0], -1, 4])
+        return [rpn_class_logits, rpn_probs, rpn_bbox]
+
+
+# Proposal Layer
+def proposal_layer(anchors, scores, deltas):
+    """
+    Receives anchor scores and selects a subset to pass as proposals to the second stage. Filtering is done based on
+    anchor scores and non-max suppression to remove overlaps. It also applies bounding box refinement deltas to anchors.
+
+    anchors: [batch, num_anchors, (y1, x1, y2, x2)] anchors in normalized coordinates
+    scores: [batch, num_anchors, (bg prob, fg prob)]
+    deltas: [batch, num_anchors, (dy, dx, log(dh), log(dw))]
+
+    return:
+    """
+
+
+
+
+# Mask R-CNN
+class MRCNN(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.resnet = ResNet(in_channels, 2048)
+        self.fpn = FPN(2048, 256)
+        self.rpn = RPN(256)
+
+    def forward(self, x):
+        resnet_feature_maps = self.resnet(x)
+        p2, p3, p4, p5, p6 = self.fpn(resnet_feature_maps)
+        rpn_feature_maps = [p2, p3, p4, p5, p6]
+        mrcnn_feature_maps = [p2, p3, p4, p5]
+
+        layer_outputs = []
+        for p in rpn_feature_maps:
+            layer_outputs.append(self.rpn(p))
+
+        return layer_outputs
+
+
+if __name__ == '__main__':
+    x = torch.rand([10, 1, 256, 256])
+    mrcnn = MRCNN(1)
+    out = mrcnn(x)
 
---
+++ Utils.py
@@ -0,0 +1,92 @@
+import numpy as np
+import math
+
+
+class AnchorGenerator:
+    def __init__(self, scales, ratios):
+        """
+        scales: tuple (scale1, scale2, ...). It means the area of the anchor will be scale^2.
+        ratios: tuple (ratio1, ratio2, ...). It means h/w of the anchor will be ratio.
+        """
+        self.scales = scales
+        self.ratios = ratios
+        self.base_anchors = self.generate_base_anchors()
+
+    def get_anchors(self, pyramid_shapes, feature_stride, anchor_stride=1):
+        """
+        The main function. To get anchors relative to different pyramid sizes.
+
+        pyramid_shapes: list [p2_shape, p3_shape, ..., p5_shape]. Every shape is interpreted in a tuple type.
+        feature_stride: int. The distance of between two pixels on original image projected from the feature map. It's
+                       relative to image_size/feature_map_size.
+        anchor_stride: int. The stride between the two pixels on feature map to get anchors.
+
+        return:
+        """
+        all_anchors = []
+        for pyramid_shape in pyramid_shapes:
+            pass
+            all_anchors.append(self.generate_pyramid_anchors(pyramid_shape, feature_stride, anchor_stride))
+        all_anchors = np.concatenate(all_anchors, axis=0)
+
+        return all_anchors
+
+    # generate base anchors
+    def generate_base_anchors(self):
+        """
+        stride: int. It means the distance between the nearby pixels in a feature map projected to the origin image.
+        scales: tuple (scale1, scale2, ...). It means the area of the anchor will be scale^2.
+        ratios: tuple (ratio1, ratio2, ...). It means h/w of the anchor will be ratio.
+
+        return: np.array [n_scales*n_ratios, 4], dtype=float. It means the base anchors of 1 pixel.
+        """
+        anchors = []
+        for scale in self.scales:
+            for ratio in self.ratios:
+                anchors.append(self.generate_1_anchor(scale, ratio))
+        anchors = np.asarray(anchors)
+
+        return anchors
+
+    def generate_1_anchor(self, scale, ratio):
+        """
+        generate 1 anchor by 1 scale and ratio.
+        Take the center of the receptive field on the origin image as origin.
+
+        return: [-w/2, -h/2, w/2, h/2] dtype=float. the coordinates of the bottom-left and upper-right point
+        of the box ith size of w and h.
+        """
+        scale = float(scale)
+        ratio = float(ratio)
+
+        # Easy to infer this formula.
+        w = scale / math.sqrt(ratio)
+        h = ratio * w
+
+        return [-w/2, -h/2, w/2, h/2]
+
+    # generate pyramid anchors
+    def generate_pyramid_anchors(self, pyramid_shape, feature_stride, anchor_stride):
+        """
+        Generate anchors according to the base anchors and the pyramid feature map shapes.
+
+        pyramid_shapes: tuple (h, w). Shape of a pyramid feature map.
+
+        return:
+        """
+        y_count, x_count = pyramid_shape
+
+        anchors = []
+        for y in range(0, y_count, anchor_stride):
+            for x in range(0, x_count, anchor_stride):
+                # feature_stride*(x,y) means the center coordinates of the target anchors. So to every single pixel on
+                # the origin image, we can just shift the base n anchors to the target center to generate new anchors.
+                shift = feature_stride * np.array([x, y, x, y])
+                anchors.append(self.base_anchors + shift)
+        anchors = np.concatenate(anchors, axis=0)
+        return anchors
+
+
+if __name__ == '__main__':
+    ag = AnchorGenerator([32, ], [0.5, 1])
+    print((ag.get_anchors([(10, 10), (20, 20)], 16)).shape)
